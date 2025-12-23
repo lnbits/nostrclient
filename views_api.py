8cb536005a65e3df7f1c0b1447f3ef18decea252
Ben Weeks <ben.weeks@outlook.com>
2025-12-23 00:35:19 +0000

    Revert "fix: improve type narrowing for mypy"
    This reverts commit cacbc67d187af80f0bbcb1da5bbf7c53e97b4437.

--- views_api.py
+++ views_api.py
@@ -172,11 +172,7 @@ async def api_get_config() -> Config:
     config = await get_config(owner_id="admin")
     if not config:
         config = await create_config(owner_id="admin")
-    if not config:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail="Failed to get or create config",
-        )
+        assert config, "Failed to create config"
     # Add private WebSocket endpoint for admin use
     config.private_ws_endpoint = encrypt_internal_message("relay", urlsafe=True)
     return config
